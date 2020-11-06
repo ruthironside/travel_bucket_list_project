16343e114276dcc4a1ebd1c09cde3e5235d6bb27
Ruth Ironside <ruthironside@live.com>
2020-11-06 16:49:46 +0000

    trying to test classes

--- console.py
+++ console.py
@@ -1,9 +1,9 @@
 import pdb
-from models.city import City
 from models.country import Country
+from models.city import City
 
-import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
+import repositories.city_repository as city_repository
 
 city_repository.delete_all()
 country_repository.delete_all()
