5df909e3fb42be5f85759948036ff78aa9aa54af
Ruth Ironside <ruthironside@live.com>
2020-11-06 16:16:05 +0000

    first commit - set up files and folders ready for population

---
+++ console.py
@@ -0,0 +1,25 @@
+import pdb
+from models.city import City
+from models.country import Country
+
+import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
+
+city_repository.delete_all()
+country_repository.delete_all()
+
+country1 = Country("France", "Europe")
+country_repository.save(country1)
+country2 = Country("Germany", "Europe")
+Country_repository.save(country2)
+
+country_repository.select_all()
+
+city_1 = City("Paris", country1, "Beautiful", True)
+city_repository.save(city_1)
+
+city_2 = City("Berlin", city2, "Amazing!", True)
+city_repository.save(city_2)
+
+
+pdb.set_trace()
