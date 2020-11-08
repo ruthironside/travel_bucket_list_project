1a9dc0e1f2216dd055e548daaf3e85488610bd94
Ruth Ironside <ruthironside@live.com>
2020-11-08 14:38:05 +0000

    fixed some html

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -8,21 +8,29 @@ import repositories.city_repository as city_repository
 countries_blueprint = Blueprint("countries", __name__)
 
 
-# GET '/countries/new'
+
 @countries_blueprint.route("/countries")
 def countries():
     countries = country_repository.select_all()
-    return render_template("countries/new.html", countries = countries)
+    return render_template("countries/index.html", countries=countries)
+
+# NEW '/countries/new'
+@countries_blueprint.route("/countries/new")
+def new_country():
+    return render_template("countries/new.html")
+
 
 # CREATE
 # POST '/countries'
 @countries_blueprint.route("/countries",  methods=['POST'])
-def new_country():
+def create_country():
     name = request.form['name']
     continent = request.form['continent']
-    country = Country(name, continent)
-    country_repository.save(country)
-    return redirect('/countries')
+    new_country = Country(name, continent,id)
+    country_repository.save(new_country)
+    # return redirect('/countries')
+    # return render_template('countries/show.html', country = country)
+    return redirect("/countries")
 
 # SHOW
 @countries_blueprint.route("/countries/<id>", methods=['GET'])
@@ -34,15 +42,16 @@ def show_country(id):
 @countries_blueprint.route("/countries/<id>/edit", methods=['GET'])
 def edit_country(id):
     country = country_repository.select(id)
-    city = country_repository.select_all()
-    return render_template('countries/edit.html', country = country, cities = cities)
+    # city = country_repository.select_all()
+    # return render_template('countries/edit.html', country = country, cities = cities)
+    return render_template('countries/edit.html', country = country)
 
 # UPDATE
 @countries_blueprint.route("/countries/<id>", methods=['POST'])
 def update_country(id):
     name = request.form['name']
     continent = request.form['continent']
-    country = Country(name, continent)
+    country = Country(name, continent, id)
     country_repository.save(country)
     return redirect('/countries')
 
